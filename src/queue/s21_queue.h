fd65ae1599cd40d084a446fc7164a15f8138e1c4
Avarfox <Libertacoro@yandex.ru>
2024-03-03 18:38:41 +0300

    stack queue extra

--- src/queue/s21_queue.h
+++ src/queue/s21_queue.h
@@ -5,142 +5,149 @@
 #include <stdexcept>
 
 namespace s21 {
-    template <typename T>
-    class queue {
-        public:
-            // type aliases
-            using value_type = T;
-            using size_type = std::size_t;
-            using reference = T&;
-            using const_reference = const T&;
+template <typename T>
+class queue {
+ public:
+  // type aliases
+  using value_type = T;
+  using size_type = std::size_t;
+  using reference = T&;
+  using const_reference = const T&;
 
-            // constructors and destructor
-            queue();
-            queue(std::initializer_list<value_type> const& items);
-            queue(const queue& q);
-            queue(queue&& q);
-            ~queue();
+  // constructors and destructor
+  queue();
+  queue(std::initializer_list<value_type> const& items);
+  queue(const queue& q);
+  queue(queue&& q);
+  ~queue();
 
-            // assignment operator
-            queue& operator=(queue &&q) {
-                if (this != &q) {
-                    swap(q);
-                }
-                return *this;
-            }
-
-            // element access
-            const_reference front();
-            const_reference back();
-
-            // capacity
-            bool empty();
-            size_type size();
-
-            // modifiers
-            void push(const_reference value);
-            void pop();
-            void swap(queue& other);
-
-        private:
-            struct node {
-                value_type data;
-                node* next;
-            };
-
-            node* head;
-            node* tail;
-            size_type sz;
-    };
-
-    // constructors and destructor
-    template <typename T>
-    queue<T>::queue() : head(nullptr), tail(nullptr), sz(0) {}
-
-    template <typename T>
-    queue<T>::queue(std::initializer_list<value_type> const& items) : queue() {
-        for (auto const& item : items) {
-            push(item);
-        }
+  // assignment operator
+  queue& operator=(queue&& q) {
+    if (this != &q) {
+      swap(q);
     }
+    return *this;
+  }
 
-    template <typename T>
-    queue<T>::queue(const queue& q) : queue() {
-        for (node* current = q.head; current != nullptr; current = current->next) {
-            push(current->data);
-        }
-    }
+  // element access
+  const_reference front();
+  const_reference back();
 
-    template <typename T>
-    queue<T>::queue(queue&& q) : queue() {
-        swap(q);
-    }
+  // capacity
+  bool empty();
+  size_type size();
 
-    template <typename T>
-    queue<T>::~queue() {
-        while (head != nullptr) {
-            pop();
-        }
-    }
+  // modifiers
+  void push(const_reference value);
+  void pop();
+  void swap(queue& other);
+  void insert_many_back(Args&&... args);
 
-    // element access
-    template <typename T>
-    typename queue<T>::const_reference queue<T>::front() {
-        if (empty()) {
-            throw std::out_of_range("queue::front(): queue is empty");
-        }
-        return head->data;
-    }
+ private:
+  struct node {
+    value_type data;
+    node* next;
+  };
 
-    template <typename T>
-    typename queue<T>::const_reference queue<T>::back() {
-        if (empty()) {
-            throw std::out_of_range("queue::back(): queue is empty");
-        }
-        return tail->data;
-    }
+  node* head;
+  node* tail;
+  size_type sz;
+};
 
-    // capacity
-    template <typename T>
-    bool queue<T>::empty() {
-        return sz == 0;
-    }
+template <typename T>
+template <typename... Args>
+void queue<T>::insert_many_back(Args&&... args) {
+  (push(std::forward<Args>(args)), ...);
+}
 
-    template <typename T>
-    typename queue<T>::size_type queue<T>::size() {
-        return sz;
-    }
+// constructors and destructor
+template <typename T>
+queue<T>::queue() : head(nullptr), tail(nullptr), sz(0) {}
 
-    // modifiers
-    template <typename T>
-    void queue<T>::push(const_reference value) {
-        node* new_node = new node{value, nullptr};
-        if (empty()) {
-            head = new_node;
-        } else {
-            tail->next = new_node;
-        }
-        tail = new_node;
-        ++sz;
-    }
+template <typename T>
+queue<T>::queue(std::initializer_list<value_type> const& items) : queue() {
+  for (auto const& item : items) {
+    push(item);
+  }
+}
 
-    template <typename T>
-    void queue<T>::pop() {
-        if (empty()) {
-            throw std::out_of_range("queue::pop(): queue is empty");
-        }
-        node* old_head = head;
-        head = head->next;
-        delete old_head;
-        --sz;
-    }
+template <typename T>
+queue<T>::queue(const queue& q) : queue() {
+  for (node* current = q.head; current != nullptr; current = current->next) {
+    push(current->data);
+  }
+}
 
-    template <typename T>
-    void queue<T>::swap(queue& other) {
-        std::swap(head, other.head);
-        std::swap(tail, other.tail);
-        std::swap(sz, other.sz);
-    }
-}   // namespace s21
+template <typename T>
+queue<T>::queue(queue&& q) : queue() {
+  swap(q);
+}
 
-#endif // S21_CONTAINERS_SRC_QUEUE_S21_QUEUE_H_
+template <typename T>
+queue<T>::~queue() {
+  while (head != nullptr) {
+    pop();
+  }
+}
+
+// element access
+template <typename T>
+typename queue<T>::const_reference queue<T>::front() {
+  if (empty()) {
+    throw std::out_of_range("queue::front(): queue is empty");
+  }
+  return head->data;
+}
+
+template <typename T>
+typename queue<T>::const_reference queue<T>::back() {
+  if (empty()) {
+    throw std::out_of_range("queue::back(): queue is empty");
+  }
+  return tail->data;
+}
+
+// capacity
+template <typename T>
+bool queue<T>::empty() {
+  return sz == 0;
+}
+
+template <typename T>
+typename queue<T>::size_type queue<T>::size() {
+  return sz;
+}
+
+// modifiers
+template <typename T>
+void queue<T>::push(const_reference value) {
+  node* new_node = new node{value, nullptr};
+  if (empty()) {
+    head = new_node;
+  } else {
+    tail->next = new_node;
+  }
+  tail = new_node;
+  ++sz;
+}
+
+template <typename T>
+void queue<T>::pop() {
+  if (empty()) {
+    throw std::out_of_range("queue::pop(): queue is empty");
+  }
+  node* old_head = head;
+  head = head->next;
+  delete old_head;
+  --sz;
+}
+
+template <typename T>
+void queue<T>::swap(queue& other) {
+  std::swap(head, other.head);
+  std::swap(tail, other.tail);
+  std::swap(sz, other.sz);
+}
+}  // namespace s21
+
+#endif  // S21_CONTAINERS_SRC_QUEUE_S21_QUEUE_H_
--- src/stack/s21_stack.h
+++ src/stack/s21_stack.h
@@ -4,123 +4,131 @@
 #include <iostream>
 
 namespace s21 {
-    template <typename T>
-    class stack {
-        public:
-            // type aliases
-            using value_type = T;
-            using size_type = std::size_t;
-            using reference = T&;
-            using const_reference = const T&;
+template <typename T>
+class stack {
+ public:
+  // type aliases
+  using value_type = T;
+  using size_type = std::size_t;
+  using reference = T&;
+  using const_reference = const T&;
 
-            // constructors and destructor
-            stack();
-            stack(std::initializer_list<value_type> const& items);
-            stack(const stack& s);
-            stack(stack&& s);
-            ~stack();
+  // constructors and destructor
+  stack();
+  stack(std::initializer_list<value_type> const& items);
+  stack(const stack& s);
+  stack(stack&& s);
+  ~stack();
 
-            // assignment operator
-            stack& operator=(stack &&s) {
-                if (this != &s) {
-                    swap(s);
-                }
-                return *this;
-            }
-
-            // element access
-            const_reference top();
-
-            // capacity
-            bool empty();
-            size_type size();
-
-            // modifiers
-            void push(const_reference value);
-            void pop();
-            void swap(stack& other);
-
-        private:
-            struct node {
-                value_type data;
-                node* next;
-            };
-
-            node* head;
-            size_type sz;
-    };
-
-    // constructors and destructor
-    template <typename T>
-    stack<T>::stack() : head(nullptr), sz(0) {}
-
-    template <typename T>
-    stack<T>::stack(std::initializer_list<value_type> const& items) : head(nullptr), sz(0) {
-        for (auto const& item : items) {
-            push(item);
-        }
+  // assignment operator
+  stack& operator=(stack&& s) {
+    if (this != &s) {
+      swap(s);
     }
+    return *this;
+  }
 
-    template <typename T>
-    stack<T>::stack(const stack& s) : head(nullptr), sz(0) {
-        for (node* current = s.head; current != nullptr; current = current->next) {
-            push(current->data);
-        }
-    }
-    
-    template <typename T>
-    stack<T>::stack(stack&& s) : head(nullptr), sz(0) {
-        swap(s);
-    }
+  // element access
+  const_reference top();
 
-    template <typename T>
-    stack<T>::~stack() {
-        while (!empty()) {
-            pop();
-        }
-    }
+  // capacity
+  bool empty();
+  size_type size();
 
-    // element access
-    template <typename T>
-    typename stack<T>::const_reference stack<T>::top() {
-        return head->data;
-    }
+  // modifiers
+  void push(const_reference value);
+  void pop();
+  void swap(stack& other);
+  void insert_many_front(Args&&... args);
 
-    // capacity
-    template <typename T>
-    bool stack<T>::empty() {
-        return sz == 0;
-    }
+ private:
+  struct node {
+    value_type data;
+    node* next;
+  };
 
-    template <typename T>
-    typename stack<T>::size_type stack<T>::size() {
-        return sz;
-    }
+  node* head;
+  size_type sz;
+};
 
-    // modifiers
-    template <typename T>
-    void stack<T>::push(const_reference value) {
-        node* new_node = new node{value, head};
-        head = new_node;
-        ++sz;
-    }
+template <typename T>
+template <typename... Args>
+void stack<T>::insert_many_front(Args&&... args) {
+  (push(std::forward<Args>(args)), ...);
+}
 
-    template <typename T>
-    void stack<T>::pop() {
-        if (empty()) {
-            return;
-        }
-        node* old_head = head;
-        head = head->next;
-        delete old_head;
-        --sz;
-    }
+// constructors and destructor
+template <typename T>
+stack<T>::stack() : head(nullptr), sz(0) {}
 
-    template <typename T>
-    void stack<T>::swap(stack& other) {
-        std::swap(head, other.head);
-        std::swap(sz, other.sz);
-    }
+template <typename T>
+stack<T>::stack(std::initializer_list<value_type> const& items)
+    : head(nullptr), sz(0) {
+  for (auto const& item : items) {
+    push(item);
+  }
+}
+
+template <typename T>
+stack<T>::stack(const stack& s) : head(nullptr), sz(0) {
+  for (node* current = s.head; current != nullptr; current = current->next) {
+    push(current->data);
+  }
+}
+
+template <typename T>
+stack<T>::stack(stack&& s) : head(nullptr), sz(0) {
+  swap(s);
+}
+
+template <typename T>
+stack<T>::~stack() {
+  while (!empty()) {
+    pop();
+  }
+}
+
+// element access
+template <typename T>
+typename stack<T>::const_reference stack<T>::top() {
+  return head->data;
+}
+
+// capacity
+template <typename T>
+bool stack<T>::empty() {
+  return sz == 0;
+}
+
+template <typename T>
+typename stack<T>::size_type stack<T>::size() {
+  return sz;
+}
+
+// modifiers
+template <typename T>
+void stack<T>::push(const_reference value) {
+  node* new_node = new node{value, head};
+  head = new_node;
+  ++sz;
+}
+
+template <typename T>
+void stack<T>::pop() {
+  if (empty()) {
+    return;
+  }
+  node* old_head = head;
+  head = head->next;
+  delete old_head;
+  --sz;
+}
+
+template <typename T>
+void stack<T>::swap(stack& other) {
+  std::swap(head, other.head);
+  std::swap(sz, other.sz);
+}
 
 }  // namespace s21
 
